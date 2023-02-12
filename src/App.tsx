dbc2a93e340837bbb1d14f611a84682faff2df26
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-12 17:06:14 +0300

    new component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./styles/index.scss";
-import Landing from './components/Landing'
+import Landing from './components/Landing';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <About />
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,32 @@ const thirdHero = require('../assets/images/aboutImages/3.png');
 const About = () => {
   return (
     <div className='aboutContainer'>
-      
+       <h1>About us</h1>
+       <div className='aboutWrapper'>
+        <div className="mentors">
+            <p>mentors</p>
+            <section>
+                <div>
+                    <img src={firstHero} alt="first mentor" />
+                    <h3>Wade Warren</h3>
+                    <p>Front-end engineers work closely with designers</p>
+                </div>
+                <div>
+                    <img src={secondHero} alt="second hero" />
+                    <h3>Kristin Watson</h3>
+                    <p>Front-end engineers work closely with designers</p>
+                </div>
+                <div>
+                    <img src={thirdHero} alt="third hero" />
+                    <h3>Robert Fox</h3>
+                    <p>Front-end engineers work closely with designers</p>
+                </div>
+            </section>
+        </div>
+        <div className="desc">
+            <p>Front-end engineers work closely with designers to make websites beautiful, functional, and fast. This Career Path will teach you not only the necessary languages and technologies, but how to think like a front-end engineer, too.</p>
+        </div>
+       </div>
     </div>
   )
 }
