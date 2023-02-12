86ffbb46773a2930bfc08fbdf11367a6bf95f729
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-12 16:30:35 +0300

    new compoent added

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const firstHero = require('../assets/images/aboutImages/1.png');
+const secondHero = require('../assets/images/aboutImages/2.png');
+const thirdHero = require('../assets/images/aboutImages/3.png');
+
+const About = () => {
+  return (
+    <div className='aboutContainer'>
+      
+    </div>
+  )
+}
+
+export default About
